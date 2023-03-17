99aea3ab95c04cab30b73cadc23374fa12f192a3
xrscode <120644276+xrscode@users.noreply.github.com>
2023-03-17 09:38:10 +0000

    Guess My Number
    Dealing with Event Listener's for the first time.

--- myscripts.js
+++ myscripts.js
@@ -1,20 +1,5 @@
 "use strict";
-console.log(document.querySelector(".message").textContent); //Select in the same way you would CSS.
-document.querySelector(".message").textContent = "Correct Number!";
-console.log(document.querySelector(".message").textContent);
+//Listen for Events by selecting the element where the event will happen;
 
-// //Score:
-// //Class = 'label-score'
-// console.log(document.querySelector(".label-score").textContent);
-// document.querySelector(".label-score").textContent = 10;
-
-// //Secret Number;
-// //Class = 'number'
-// console.log(document.querySelector(".number").textContent);
-// document.querySelector(".number").textContent = 13;
-
-// //Input Field;
-// //Class = 'guess'
-// console.log(document.querySelector(".guess").value);
-// document.querySelector(".guess").value = 23;
-// console.log(document.querySelector(".guess").value);
+//The Check Button
+document.querySelector(".check");
