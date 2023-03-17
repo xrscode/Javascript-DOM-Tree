e4a00e41debcb70fc9be8b007bdc6020a24704d6
xrscode <120644276+xrscode@users.noreply.github.com>
2023-03-17 10:19:59 +0000

    Check Button
    Implementing logic to check a user has entered a value.

--- myscripts.js
+++ myscripts.js
@@ -2,4 +2,11 @@
 //Listen for Events by selecting the element where the event will happen;
 
 //The Check Button
-document.querySelector(".check");
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(typeof guess, guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = `No Number! 🙈`;
+  }
+});
