58be6a7d38e8cfbb81191b8b66c663afdd841ff2
xrscode <120644276+xrscode@users.noreply.github.com>
2023-04-07 10:01:32 +0100

    Update script.js

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -1,1 +1,13 @@
-'use strict';
+"use strict";
+let dice = Math.ceil(Math.random() * 6);
+
+//Roll Dice
+document.querySelector('.dice').addEventListener('click'), function () {
+
+};
+
+
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
