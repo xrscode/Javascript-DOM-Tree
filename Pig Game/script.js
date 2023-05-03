1d3a53f98e0852073ebe7200367802c673959959
xrscode <120644276+xrscode@users.noreply.github.com>
2023-05-03 10:53:07 +0100

    Pig Game

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -1,13 +1,37 @@
 "use strict";
-let dice = Math.ceil(Math.random() * 6);
+//Selecting Elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 
-//Roll Dice
-document.querySelector('.dice').addEventListener('click'), function () {
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
-};
+//Starting Conditions; Score 0.
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
 
+let currentScore = 0;
 
+//Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  //1.  Generate random dice roll.
+  let dice = Math.ceil(Math.random() * 6);
 
-document.querySelector('.check').addEventListener('click', function () {
-    const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
+  //2. Display dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+
+  //3. Check for rolled 1.  If true, switch to next player.
+  if (dice !== 1) {
+    //If dice score is NOT 1, add number to current score.
+    currentScore += dice;
+    current0El.textContent = currentScore; //Change Later
+  } else {
+    //Stay on player.
+  }
+});
